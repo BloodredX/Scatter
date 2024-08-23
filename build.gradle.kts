c10c3f2a5b14bd758726ebe0d27f4e67dbe66b90
Bloodred <89779399+BloodredX@users.noreply.github.com>
2024-08-23 06:15:02 +0530

    Rebranding to Scatter

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/") {
+    maven("https://repo.papermc.io/repository/maven-public/") {
         content { onlyForConfigurations(PAPERCLIP_CONFIG) }
     }
 }
